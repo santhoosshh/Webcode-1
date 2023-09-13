062d280a44d6503faa14760f934100132cde7c89
santhoosshh <123050655+santhoosshh@users.noreply.github.com>
2023-09-13 13:13:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const videoContainer = document.querySelector(".video-container");
+
+let api_key = "AIzaSyCipL2ply7hXs_rMWQ7RLP1VvHO8XULy78";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regipncode: 'IN' 
+})).then(res => res.json())
+.then(data =>{
+    data.items.forEach(item => {
+    getChannelIcon(item)
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    })).then(res => res.json())
+    .then(data =>{
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+const makeVideoCard = (data) => {
+    videoContainer.innerHTML +=`<div class = "video" onclick= "location.href = 'https://youtube.com/watch?v=v${data.id}'">
+    <img src = "${data.snippet.thumbnails.high.url}" class="thumbnail" alt=">
+    <div class="content">
+    <img src = "${data.channelThumbnail}" class="channel-icon" alt="">
+    <div class = "info">
+    <h4 class = "title"> ${data.snippet.title} </h4>
+    <p class = "channel-name> ${data.snippet.channelTitle} </p>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+// search 
+
+const searchInput = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
+
+let searchLink = "https://www.youtube.com/results?search_qurey=";
+searchBtn.addEventListener("click", () => {
+    if(searchInput.Value.length){
+        location.href = searchLink + searchInput.Value;
+    }
+})
